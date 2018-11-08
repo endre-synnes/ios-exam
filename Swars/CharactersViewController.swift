ef3ff6a1da968ffdbe7c59a168380d5fef3652a0
Endre Synnes <synend16@student.westerdals.no>
2018-11-08 17:08:18 +0100

    initial ommit with some setup

---
+++ Swars/CharactersViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CharactersViewController.swift
+//  Swars
+//
+//  Created by Endre Mikal Synnes on 08/11/2018.
+//  Copyright © 2018 Endre Mikal Synnes. All rights reserved.
+//
+
+import UIKit
+
+class CharactersViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
