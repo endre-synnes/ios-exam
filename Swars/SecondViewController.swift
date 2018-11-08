c05ead39552e5410dd8c30719cb1dd078cf575f0
Endre Synnes <synend16@student.westerdals.no>
2018-11-08 15:22:24 +0100

    Initial Commit

---
+++ Swars/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Swars
+//
+//  Created by Endre Mikal Synnes on 08/11/2018.
+//  Copyright © 2018 Endre Mikal Synnes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
